3078fb876a3408d6d8c489ffe72fc7d9283588ab
Afsha10 <unityofbeing1@gmail.com>
2024-01-29 23:44:56 +0000

    Lesson 1
    * print sum of two numbers
    * print an item in a specific position from an array of values
    * print a calculated value combining the above examples

---
+++ main.py
@@ -0,0 +1,36 @@
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+my_first_variable = 45
+
+print (my_first_variable)
+
+##############################
+
+num1 = 12
+num2 = 15
+
+result = num1 + num2
+
+##############################
+
+value1 = 12
+value2 = 13
+value3 = 13.5
+value4 = 56
+
+list1 = [value1, value2, value3, value4]
+
+print(list1[1])
+print(list[-1])
+
+
+##############################
+
+inputs = [1.2, 5.1, 2.1]
+weights = [3.1, 2.1, 8.7]
+bias = 3.0
+ 
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+print(output)
+
+##############################
